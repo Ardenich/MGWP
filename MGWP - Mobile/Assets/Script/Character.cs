e0d2f41687b7c1770f18ea4881083a88a402a74c
Ardenich <36920567+Ardenich@users.noreply.github.com>
2018-12-04 21:37:12 +0700

    ASDASD

---
+++ MGWP - Mobile/Assets/Script/Character.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour {
+
+    public GameObject spawnPoint;
+    public Text pointText;
+    public int points = 0;
+    public bool done = false;
+    public GameObject[] balls;
+    public JoyStickScript joystick;
+
+    Command commandScript;
+    Rigidbody2D rb;
+    float movSpeed = 3;
+    Animator anim;
+    Vector2 direction;
+
+
+    // Use this for initialization
+    void Start() {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        this.gameObject.transform.position = spawnPoint.transform.position;
+        commandScript = spawnPoint.GetComponent<Command>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.rotation = Quaternion.identity;
+        pointText.text = points.ToString();
+
+        if(joystick.Horizontal() == 0 && joystick.Vertical() == 0)
+        {
+            direction = Vector2.zero;
+        }
+        else
+        {
+            direction.x = joystick.Horizontal();
+            direction.y = joystick.Vertical();
+        }
+
+        rb.velocity = new Vector2(direction.x * movSpeed, direction.y * movSpeed);
+
+        if (direction.y >= 0.1f)
+        {
+            anim.SetBool("up", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+            anim.SetBool("down", false);
+        }
+        if (direction.y <= -0.1f)
+        {
+            anim.SetBool("down", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+            anim.SetBool("up", false);
+        }
+        if (direction.x >= 0.1f)
+        {
+            anim.SetBool("right", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("left", false);
+            anim.SetBool("up", false);
+            anim.SetBool("down", false);
+        }
+        if (direction.x <= -0.1f)
+        {
+            anim.SetBool("left", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("up", false);
+            anim.SetBool("right", false);
+            anim.SetBool("down", false);
+        }
+        if(rb.velocity == Vector2.zero)
+        {
+            anim.SetBool("moving", false);
+        }
+
+        /*if (Input.GetKey("w"))
+        {
+            anim.SetBool("up", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+            anim.SetBool("down", false);
+            rb.velocity = new Vector2(rb.velocity.x, movSpeed);
+        }
+        if (Input.GetKey("s"))
+        {
+            anim.SetBool("down", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+            anim.SetBool("up", false);
+            rb.velocity = new Vector2(rb.velocity.x, -movSpeed);
+        }
+        if (Input.GetKey("a"))
+        {
+            anim.SetBool("left", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("up", false);
+            anim.SetBool("right", false);
+            anim.SetBool("down", false);
+            rb.velocity = new Vector2(-movSpeed, rb.velocity.y);
+        }
+        if (Input.GetKey("d"))
+        {
+            anim.SetBool("right", true);
+            anim.SetBool("moving", true);
+            anim.SetBool("left", false);
+            anim.SetBool("up", false);
+            anim.SetBool("down", false);
+            rb.velocity = new Vector2(movSpeed, rb.velocity.y);
+        }
+        if (!Input.anyKey)
+        {
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0;
+            anim.SetBool("moving", false);
+        }*/
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            if (commandScript.arrayNum == 0 && collision.gameObject.name == "Blue Ball")
+            {
+                endPoints();
+            }
+            if (commandScript.arrayNum == 1 && collision.gameObject.name == "Black Ball")
+            {
+                endPoints();
+            }
+            if (commandScript.arrayNum == 2 && collision.gameObject.name == "Green Ball")
+            {
+                endPoints();
+            }
+        }
+    }
+
+    void endPoints()
+    {
+        done = true;
+        points++;
+        if (commandScript.timer > 0)
+        {
+            balls[commandScript.arrayNum].SetActive(false);
+        }
+        if (commandScript.timer < 0 || done == true)
+        {
+            balls[commandScript.arrayNum].SetActive(true);
+            done = false;
+            commandScript.Ended();
+        }
+    }
+
+}
---
+++ MGWP - Mobile/Assets/Script/Command.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Command : MonoBehaviour {
+
+    string[] commandsArray = new string[3];
+
+    public int arrayNum;
+    public float timer;
+    public float timeLimit = 10f;
+
+    public Text commandText;
+    public Text timeText;
+
+	// Use this for initialization
+	void Start () {
+        commandsArray[0] = "Take Blue Ball";
+        commandsArray[1] = "Take Black Ball";
+        commandsArray[2] = "Take Green Ball";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeText.text = FormatTimer(timer);
+        if (timer == timeLimit)
+        {
+            arrayNum = Random.Range(0, commandsArray.Length);
+        }
+        timer -= Time.deltaTime;
+        if (timer > 0)
+        {
+            commandText.text = commandsArray[arrayNum];
+        }
+        if (timer < 0)
+        {
+            Ended();
+        }
+	}
+
+    public void Ended()
+    {
+        timeLimit -= Random.Range (-2f, 2f);
+        timer = timeLimit;
+    }
+    public string FormatTimer(float timer)
+    {
+        float totalTime = timer;
+        if (timer > 0)
+        {
+            int minutes = (int)(totalTime / 60) % 60;
+            int seconds = (int)totalTime % 60;
+
+            string time = string.Format("{0:00}:{1:00}", minutes, seconds);
+            return time;
+        } else
+        {
+            int minutes = (int)(totalTime / 60) % 60;
+            int seconds = ((int)totalTime % 60) * -1;
+
+            string time = string.Format("{0:0}", seconds);
+            return time;
+        }
+    }
+}
